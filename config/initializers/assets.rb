af67e50b42d5aea317b5bf085e1bac051a11260a
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-04-20 15:52:19 -0300

    Load SB Admin Theme

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w[
+  theme/chart.js
+  theme/datatables.js
+  theme/easing.js
+  theme/sidebar.js
+]
