5d35512dfe6a96e77479014662a73b0383eb3fcb
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-05-04 10:34:02 -0300

    Added Records to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_181834) do
+ActiveRecord::Schema.define(version: 2019_05_04_131245) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "records", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schema_info", id: false, force: :cascade do |t|
+    t.integer "version", default: 0, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
