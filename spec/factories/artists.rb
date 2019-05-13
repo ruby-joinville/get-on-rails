4f58f5a9d3c1b994a314f481d0c11501d991c9c4
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-05-13 17:25:43 -0300

    Create user on artist factory

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :artist do
     name { Faker::Movies::HitchhikersGuideToTheGalaxy.character }
     profile { Faker::Movies::HitchhikersGuideToTheGalaxy.quote }
+
+    user
   end
 end
