591f5ac17da61af3b2691627bbfc3c89b9f0fd2b
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-06-06 20:35:24 -0300

    Fixed minors

--- spec/services/discogs_requests_service_spec.rb
+++ spec/services/discogs_requests_service_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe DiscogsRequestsService, type: :service do
   describe '#search_artist_releases' do
-    it 'gets an artist releases sussessfully' do
-      VCR.use_cassette("get_artist_the_who_releases") do
+    it 'gets an artist releases successfully' do
+      VCR.use_cassette('get_artist_the_who_releases') do
         response = described_class::Client.new.search_artist_releases('The Who')
 
         expect(response.first.title).to eq("I Can't Explain")
@@ -12,8 +12,8 @@ RSpec.describe DiscogsRequestsService, type: :service do
   end
 
   describe '#get_artist' do
-    it 'gets an artist sussessfully' do
-      VCR.use_cassette("get_artist_the_who") do
+    it 'gets an artist successfully' do
+      VCR.use_cassette('get_artist_the_who') do
         response = described_class::Client.new.get_artist('The Who')
 
         expect(response.name).to eq('The Who')
