b643ea34bfbee307ffc99bd465339c65c9c03fe4
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-06-06 21:16:58 -0300

    Added more test cases

--- spec/services/discogs_requests_service_spec.rb
+++ spec/services/discogs_requests_service_spec.rb
@@ -1,22 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe DiscogsRequestsService, type: :service do
-  describe '#search_artist_releases' do
-    it 'gets an artist releases successfully' do
-      VCR.use_cassette('get_artist_the_who_releases') do
-        response = described_class::Client.new.search_artist_releases('The Who')
+  subject(:discogs_client) { described_class::Client.new }
 
-        expect(response.first.title).to eq("I Can't Explain")
+  describe '#search_artist_releases' do
+    context 'when artist exists' do
+      it 'gets an artist releases successfully' do
+        VCR.use_cassette('services/discogs/get_artist_the_who_releases') do
+          response = discogs_client.search_artist_releases('The Who')
+
+          expect(response.first.title).to eq("I Can't Explain")
+        end
+      end
+    end
+
+    context 'when artist does not exists' do
+      it 'returns an empty hash' do
+        VCR.use_cassette('services/discogs/get_artist_gdjey537bdgfajj1wqe') do
+          response = discogs_client.search_artist_releases('gdjey537bdgfajj1wqe')
+
+          expect(response).to eq({})
+        end
       end
     end
   end
 
   describe '#get_artist' do
-    it 'gets an artist successfully' do
-      VCR.use_cassette('get_artist_the_who') do
-        response = described_class::Client.new.get_artist('The Who')
+    context 'when artist exists' do
+      it 'gets an artist successfully' do
+        VCR.use_cassette('services/discogs/get_artist_the_who') do
+          response = discogs_client.get_artist('The Who')
 
-        expect(response.name).to eq('The Who')
+          expect(response.name).to eq('The Who')
+        end
+      end
+    end
+
+    context 'when artist does not exists' do
+      it 'returns an empty hash' do
+        VCR.use_cassette('services/discogs/get_artist_tegdcjbg4vb4422nann') do
+          response = discogs_client.get_artist('tegdcjbg4vb4422nann')
+
+          expect(response).to eq({})
+        end
       end
     end
   end
