0ee7bae473b8778bf0d48d4bf3df5c69c24643cf
Christopher Renkavieski <chris.renka@gmail.com>
2019-06-08 16:02:51 -0300

    Created releases CRUD

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
-    email 'email@email.com'
+    email {Faker::Internet.email}
 
     after(:build) { |u| u.password_confirmation = u.password = 'passwd' }
   end
 
    factory :other_user, class: User do
-    email 'other_email@email.com'
+    email {Faker::Internet.email}
 
     after(:build) { |u| u.password_confirmation = u.password = 'passwd' }
   end
