a81f4a1f60582b405c4d7946aa58a2dfb5835be4
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-06-13 19:25:47 -0300

    Releases new and create actions

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -9,4 +9,29 @@ class ReleasesController < ApplicationController
     @artist = Artist.find(params[:artist_id])
     @release = Release.find(params[:id])
   end
+
+  def new
+    @artist = Artist.find(params[:artist_id])
+    @release = Release.new(artist: @artist)
+  end
+
+  def create
+    @release = Release.new(release_params)
+    @artist = Artist.find(params[:artist_id])
+    @release.artist = @artist
+
+    if @release.save
+      redirect_to artist_release_url(artist_id: @artist.id, id: @release.id), notice: 'Release was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def release_params
+    params.require(:release).permit(
+      :title
+    )
+  end
 end
