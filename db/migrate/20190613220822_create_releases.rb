6930967fc9700649a43fa5557a6776bc8399b23a
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-06-13 19:11:54 -0300

    Created releases model and relations

---
+++ db/migrate/20190613220822_create_releases.rb
@@ -0,0 +1,14 @@
+class CreateReleases < ActiveRecord::Migration[5.2]
+  def change
+    create_table :releases do |t|
+      t.string :title, null: false
+      t.string :release_type
+      t.integer :year
+      t.integer :discog_id
+
+      t.timestamps
+
+      t.belongs_to :artist, index: true, null: false
+    end
+  end
+end
