04447abe56d9ce9731bb07f1a5bdcb2bc8bc92d2
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-06-13 20:19:27 -0300

    Import artist from Discogs

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,3 +1,24 @@
 class ImportsController < ApplicationController
-  def artist; end
+  def artist
+    if import_artist_params[:name].blank?
+      redirect_to artists_path, notice: 'Please inform an artist'
+
+      return
+    end
+
+    client = DiscogsRequestsService::Client.new
+    artist = client.get_artist(import_artist_params[:name])
+
+    Artist.new(name: artist.name, profile: artist.profile, user: current_user).save
+
+    redirect_to artists_path
+  end
+
+  private
+
+  def import_artist_params
+    params.require(:import_artist).permit(
+      :name
+    )
+  end
 end
