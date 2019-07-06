1937640b131c893ae4b528c1867b6a6deb4f1fff
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-07-06 11:07:43 -0300

    Missed two bangs

--- spec/tutorials/discogs_api_tutorial/models/release_spec.rb
+++ spec/tutorials/discogs_api_tutorial/models/release_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Release, type: :model do
   let(:artist) { create(:artist) }
-  let!(:release) { create(:release, artist: artist)}
 
   it 'belongs to artist' do
+    release = create(:release, artist: artist)
+
     expect(release.artist).to eq(artist)
   end
 end
