4fe151cbfc157bfe0854686c49cd081c5481242c
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-07-06 11:09:33 -0300

    Missed other two bangs

--- spec/tutorials/sidekiq_tutorial/models/user_spec.rb
+++ spec/tutorials/sidekiq_tutorial/models/user_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
-  let!(:artist) { create(:artist, user: user) }
 
   it 'has many artists' do
+    artist = create(:artist, user: user)
+
     expect(user.artists.first).to eq(artist)
   end
 end
